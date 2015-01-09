9dc2368a72316d2530de770f724ecb663b1863f4
leafypixiestix <justincai.lps@gmail.com>
2015-01-09 11:03:49 -0700

    Initial commit, contains a basic grade calculator

---
+++ OpenSourceProject/GradeCalculator.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace OpenSourceProject
+{
+    partial class GradeCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.multiplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ptsPoss = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.percent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plannerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gradeCalculatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editCategory = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.divider2 = new System.Windows.Forms.Label();
+            this.divider1 = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelPtsPoss = new System.Windows.Forms.Label();
+            this.labelPercent = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.editCategory.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.multiplier,
+            this.ptsPoss,
+            this.score,
+            this.percent});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(542, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.onEndCellEdit);
+            // 
+            // name
+            // 
+            this.name.Frozen = true;
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // multiplier
+            // 
+            this.multiplier.Frozen = true;
+            this.multiplier.HeaderText = "Multiplier";
+            this.multiplier.Name = "multiplier";
+            // 
+            // ptsPoss
+            // 
+            this.ptsPoss.Frozen = true;
+            this.ptsPoss.HeaderText = "PtsPoss";
+            this.ptsPoss.Name = "ptsPoss";
+            // 
+            // score
+            // 
+            this.score.Frozen = true;
+            this.score.HeaderText = "Score";
+            this.score.Name = "score";
+            // 
+            // percent
+            // 
+            this.percent.Frozen = true;
+            this.percent.HeaderText = "Percent";
+            this.percent.Name = "percent";
+            this.percent.ReadOnly = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.plannerToolStripMenuItem,
+            this.gradeCalculatorToolStripMenuItem,
+            this.calculatorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(620, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // plannerToolStripMenuItem
+            // 
+            this.plannerToolStripMenuItem.Name = "plannerToolStripMenuItem";
+            this.plannerToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.plannerToolStripMenuItem.Text = "Planner";
+            // 
+            // gradeCalculatorToolStripMenuItem
+            // 
+            this.gradeCalculatorToolStripMenuItem.Name = "gradeCalculatorToolStripMenuItem";
+            this.gradeCalculatorToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.gradeCalculatorToolStripMenuItem.Text = "Grade Calculator";
+            // 
+            // calculatorToolStripMenuItem
+            // 
+            this.calculatorToolStripMenuItem.Name = "calculatorToolStripMenuItem";
+            this.calculatorToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.calculatorToolStripMenuItem.Text = "Calculator";
+            // 
+            // editCategory
+            // 
+            this.editCategory.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.editCategory.Name = "editCategory";
+            this.editCategory.Size = new System.Drawing.Size(95, 26);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.divider2);
+            this.groupBox1.Controls.Add(this.divider1);
+            this.groupBox1.Controls.Add(this.labelScore);
+            this.groupBox1.Controls.Add(this.labelPtsPoss);
+            this.groupBox1.Controls.Add(this.labelPercent);
+            this.groupBox1.Location = new System.Drawing.Point(271, 196);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 39);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Category Totals (%)";
+            // 
+            // divider2
+            // 
+            this.divider2.AutoSize = true;
+            this.divider2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.divider2.Location = new System.Drawing.Point(197, 16);
+            this.divider2.Name = "divider2";
+            this.divider2.Size = new System.Drawing.Size(2, 15);
+            this.divider2.TabIndex = 3;
+            // 
+            // divider1
+            // 
+            this.divider1.AutoSize = true;
+            this.divider1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.divider1.Location = new System.Drawing.Point(101, 16);
+            this.divider1.Name = "divider1";
+            this.divider1.Size = new System.Drawing.Size(2, 15);
+            this.divider1.TabIndex = 4;
+            // 
+            // labelScore
+            // 
+            this.labelScore.Location = new System.Drawing.Point(101, 16);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(98, 13);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelPtsPoss
+            // 
+            this.labelPtsPoss.Location = new System.Drawing.Point(6, 16);
+            this.labelPtsPoss.Name = "labelPtsPoss";
+            this.labelPtsPoss.Size = new System.Drawing.Size(89, 13);
+            this.labelPtsPoss.TabIndex = 0;
+            this.labelPtsPoss.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelPercent
+            // 
+            this.labelPercent.Location = new System.Drawing.Point(197, 16);
+            this.labelPercent.Name = "labelPercent";
+            this.labelPercent.Size = new System.Drawing.Size(96, 13);
+            this.labelPercent.TabIndex = 2;
+            this.labelPercent.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // GradeCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 252);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GradeCalculator";
+            this.Text = "Grade Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.editCategory.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn multiplier;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ptsPoss;
+        private System.Windows.Forms.DataGridViewTextBoxColumn score;
+        private System.Windows.Forms.DataGridViewTextBoxColumn percent;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plannerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gradeCalculatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculatorToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip editCategory;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label divider2;
+        private System.Windows.Forms.Label divider1;
+        private System.Windows.Forms.Label labelPercent;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelPtsPoss;
+    }
+}
+
---
+++ OpenSourceProject/GradeCalculator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSourceProject {
+    public partial class GradeCalculator : Form {
+
+        public GradeCalculator()
+        {
+            InitializeComponent();
+        }
+
+
+        private void onEndCellEdit(object sender, DataGridViewCellEventArgs e) {
+            validateEntry(sender, e);
+            calculatePercentage(sender, e);
+            calculateTotals(sender, e);
+        }
+
+        private void calculateTotals(object sender, DataGridViewCellEventArgs e) {
+            bool hasScores = false;
+            double ptsPoss = 0, score = 0;
+            DataGridView data = (DataGridView)sender;
+            for (int i = 0; i < data.Rows.Count; i++) {
+                //Check to see if there is multiplier
+                if (data.Rows[i].Cells[1].Value != null)
+                {
+                    //Update with the multiplier
+                    ptsPoss += Convert.ToDouble(data.Rows[i].Cells[2].Value) * Convert.ToDouble(data.Rows[i].Cells[1].Value);
+                    score += Convert.ToDouble(data.Rows[i].Cells[3].Value) * Convert.ToDouble(data.Rows[i].Cells[1].Value);
+                }
+                else {
+                    //Update without the multiplier (multiplier of 1)
+                    ptsPoss += Convert.ToDouble(data.Rows[i].Cells[2].Value);
+                    score += Convert.ToDouble(data.Rows[i].Cells[3].Value);
+                }
+            }
+            if (ptsPoss != 0 ) { 
+                //Update the final scores
+                labelPtsPoss.Text = ptsPoss.ToString();
+                labelScore.Text = score.ToString();
+                labelPercent.Text = (score / ptsPoss * 100).ToString() + "%";
+            }
+        }
+
+        private void validateEntry(object sender, DataGridViewCellEventArgs e) {
+            DataGridView data = (DataGridView)sender;
+            //Check if the user is in any other column other than the first
+            if (data.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null) {
+                if (e.ColumnIndex != 0)
+                {
+                    //Check if the user is entering anything other than 0-9 or a "."
+                    if (System.Text.RegularExpressions.Regex.IsMatch(data.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString(), "[^0-9.]"))
+                    {
+                        //Delete the cell
+                        data.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = null;
+                        bool rowIsEmpty = true;
+                        //Check to see if the row is empty, if it is delete the row, otherwise don't
+                        foreach (DataGridViewCell cell in data.Rows[e.RowIndex].Cells)
+                        {
+                            if (cell.Value != null)
+                            {
+                                rowIsEmpty = false;
+                                break;
+                            }
+                        }
+                        if (rowIsEmpty)
+                        {
+                            data.Rows.Remove(data.Rows[e.RowIndex]);
+                        }
+                        MessageBox.Show("Please enter only numbers.");
+                    }
+                }
+            }     
+        }
+
+        private void calculatePercentage(object sender, DataGridViewCellEventArgs e) { 
+            DataGridView data = (DataGridView)sender;
+            //Check if ptsposs or score isn't empty
+            if (data.Rows[e.RowIndex].Cells[2].Value != null && data.Rows[e.RowIndex].Cells[3].Value != null)
+            {
+                //If the ptsposs is not zero, update the table (this prevents division by 0)
+                if (Convert.ToDouble(data.Rows[e.RowIndex].Cells[2].Value) != 0) {
+                    data.Rows[e.RowIndex].Cells[4].Value = Convert.ToDouble(data.Rows[e.RowIndex].Cells[3].Value) / Convert.ToDouble(data.Rows[e.RowIndex].Cells[2].Value) * 100;
+                }
+            }
+        }
+    }
+}
